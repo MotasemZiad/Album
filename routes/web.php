7b0b671e4b156562183de5bf77ded2c98758f058
Motasem Z. AbuNema <52855540+MotasemZiad@users.noreply.github.com>
2022-03-13 17:11:21 +0200

    update the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/details', function(){
+//     return view('details');
+// });
+
+Route::get('/', [PostController::class, 'index']);
+Route::get('/details', [PostController::class, 'detailsPost']);
+
+
